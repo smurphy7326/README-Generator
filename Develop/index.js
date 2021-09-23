ad7609222f1f38caf2ee4d6fcf107b3c0521ff99
smurphy7326 <smurphy7326@gmail.com>
2021-09-23 17:42:07 -0400

    first few questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [     // help form the inquier.js from calss and the moduel 9 class activities
+    {
+        type: 'input', // This is when you type in your response 
+        name: 'projectTitle', 
+        message: 'Enter a title for your project', // This is the message that will appear in the terminal box
+        validate: titleName => {
+            if (titleName) {
+                return true; // If this is valid and you entered a title this will appear
+            } else {
+                console.log('Please enter a title for the project!'); // If nothing is entered the message will appear 
+                return false; 
+            }
+        }
+    },
+
+    {
+        type: 'input', // the type of input where you can write your own answers
+        name: 'projectDescription',
+        message: 'Write a breif message of the project.', // writes a description of the project
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please provide a description of the project.') // 
+                return false;
+            }
+        }
+    }
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required tpo install the project?',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please enter the installation instructions for your project!');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # README-Generator
 # README-Generator
 # readme-generator
+# readme-generator
